96f9a4182330e17fd2eb1a40bf2e2752f7d16b12
Quezon Province Programmer, Developers <quezon.province.pd@gmail.com>
2025-09-10 22:14:40 +0800

    Added testing logic

--- source/arduino/BoardSimulator/BoardSimulator.ino
+++ source/arduino/BoardSimulator/BoardSimulator.ino
@@ -19,5 +19,26 @@ void setup() {
 }
 
 void loop() {
- 
+  if (Serial.available() > 0) {
+    String cmd = Serial.readStringUntil('\n');
+    switch (cmd) {
+      case "LCD": // Test LCD
+        lcd.clear();
+        lcd.print("LCD Test OK");
+        Serial.println("LCD: Message displayed");
+        break;
+      case 'FLAME': { // Test FlameSensor
+        int analogVal = flameSensor.readAnalog();
+        bool digitalVal = flameSensor.readDigital();
+        Serial.print("FlameSensor Analog: ");
+        Serial.print(analogVal);
+        Serial.print(", Digital: ");
+        Serial.println(digitalVal ? "HIGH" : "LOW");
+        break;
+      }
+      default:
+        Serial.println("Unknown command. Use 'L' for LCD, 'F' for FlameSensor.");
+        break;
+    }
+  }
 }
